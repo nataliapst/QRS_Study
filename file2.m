d0ae86e25cd383b9bd9b071592a366a1073e0d61
nataliapst <79333938+nataliapst@users.noreply.github.com>
2023-03-01 11:05:07 +0100

    Add files via upload

---
+++ file2.m
@@ -0,0 +1,3 @@
+file='/Users/nataliap.st./Documents/OpenSignals (r)evolution/temp/opensignals_98D3A1FD5C27_2022-05-23_20-20-30.txt';
+%file='/Users/nataliap.st./Desktop/grabacionesEcg en txt/opensignals_98D3A1FD5C27_2022-02-23_12-35-34.txt';
+
