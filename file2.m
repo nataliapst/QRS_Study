067f287162ec5e4e015193e80c75d0761c6faf11
nataliapst <79333938+nataliapst@users.noreply.github.com>
2023-04-26 17:51:08 +0200

    Update file2.m
    Signed-off-by: nataliapst <79333938+nataliapst@users.noreply.github.com>

--- file2.m
+++ file2.m
@@ -1,3 +1,2 @@
-file='/Users/nataliap.st./Documents/OpenSignals (r)evolution/temp/opensignals_98D3A1FD5C27_2022-05-23_20-20-30.txt';
-%file='/Users/nataliap.st./Desktop/grabacionesEcg en txt/opensignals_98D3A1FD5C27_2022-02-23_12-35-34.txt';
-
+file1='/Users/nataliap.st./Desktop/IngBiomédica/Proyectos II/subjects ECG/subject64.txt';
+BITalinoFileReader(file1)
