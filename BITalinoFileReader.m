a3f379fef83eb848d416450e1a22c86174c28d68
nataliapst <79333938+nataliapst@users.noreply.github.com>
2023-06-20 19:15:47 +0200

    Update BITalinoFileReader.m
    Changes for the detection of the Q peak.
    
    Signed-off-by: nataliapst <79333938+nataliapst@users.noreply.github.com>

--- BITalinoFileReader.m
+++ BITalinoFileReader.m
@@ -1,4 +1,4 @@
-function[data,t,header, maxpksR, heartbeats] = BITalinoFileReader(file)
+function[data,t,header, maxpksR, heartbeats, amplitud] = BITalinoFileReader(file)
 
     %addpath('./jsonlab')
     %to convert matlab->java or java->matlab
@@ -25,8 +25,7 @@ function[data,t,header, maxpksR, heartbeats] = BITalinoFileReader(file)
 
    [p,s,mu] = polyfit((1:numel(ecgReal))',ecgReal,10);
     f_y = polyval(p,(1:numel(ecgReal))',[],mu);
-    %print(f_y)
-    ECG_data = (ecgReal - f_y); % esto es z score o normal distribution???
+    ECG_data = (ecgReal - f_y);
     
 
     %LOWPASS FILTER THAT REDUCES THE NOISE
@@ -58,18 +57,11 @@ um_x= 0.5*1000; %CORROBORAR SI ES 1100
 %[pksQ, locs_Qwave]= findpeaks(-ECG_data_update(25000:35000),'MinPeakWidth',60,'MaxPeakWidth',120);
 %findpeaks(-ECG_data_update(25000:35000),tiempo(25000:35000), 'MinPeakProminence',0.1);
 
-
 % - - - - - - - - - - - - - - - - - - - - - 
 
 
-[pksAll, locs_Allwaves] = findpeaks(-ECG_data_update(25000:35000));
+[pksAll, locs_Allwaves] = findpeaks(ECG_data_update(25000:35000));
 %Aqui vamos a guardar todos los máximos que encuentra la señal invertida en el eje x
-
-[pksR_inv, locs_Rwave_inv] = findpeaks(-ECG_data_update(25000:35000), 'MinPeakWidth',10,'MinPeakDistance',um_x);
-%Damos la vuelta en el eje x a la señal, para que se nos quede la Q y la S
-%como máximos y los segmentos R como minimos. Y en esta función en específico localizamos las
-%posiciones en las que se encuentra la R, en la señal invertida.
-
 
 pksQ = [];
 locs_Qwave = [];
@@ -77,9 +69,9 @@ widthQ=[];
 
 
 for j = 2:numel(pksAll)
-    for i = 1:numel(pksR_inv)
+    for i = 1:numel(pksR)
 
-        if (pksAll(j)==pksR_inv(i))
+        if (pksAll(j)==pksR(i))
     
             pksQ = [pksQ; (pksAll(j-1))];
             locs_Qwave = [locs_Qwave; (locs_Allwaves(j-1))];
@@ -90,66 +82,13 @@ for j = 2:numel(pksAll)
 end;
 
 for a = 1:numel(locs_Qwave)
-    widthQ = [widthQ; (locs_Rwave_inv(a)-locs_Qwave(a))];
+    widthQ = [widthQ; (locs_Rwave(a)-locs_Qwave(a))];
 end
+
 
 %Aqui lo que hacemos es escoger el máximo que haya anterior al segmento R,
 %de manera que realmente estamos guardando los picos Q.
 
-
-% Este era mi intento de conseguir el segmento Q, amos a decir, de manera
-% más rigurosa, fijándome en información de distintos papers, como en la
-% que el segmento Q tiene que durar aproximadamente entre
-% 0.06-0.12segundos, que tiene que ser aprox el 0.25% de la señal R.
-% También hay una función que cogi de internet que decia que la onda Q se
-% encuentra entre -0.2mV and -0.5mV
- 
-            %     Buscamos los picos de Q que estarán entre -0.2mV and -0.5mV
-            %     [pksQ,min_locs] = findpeaks(-ECG_data,'MinPeakDistance',40);
-            %     locs_Qwave = min_locs(ECG_data(min_locs)>-0.5 & ECG_data(min_locs)<-0.2);
-            %     %disp(pksQ)          
-
-%Aunque la segunda función no sale
-
-
-% cont=1;
-% real_pksQ = [];
-% real_locsQ_wave = [];
-% 
-% for a = 1:numel(pksQ)
-%     if (pksQ(a) < maxPksQ)
-%         real_pksQ = [real_pksQ; pksQ(a)];
-%         real_locsQ_wave = [real_locsQ_wave; locs_Qwave(a)];
-%     end
-% end
-% 
-% 
-% %maxSignal = [];
-% realQvalue = [];
-% value= 0;
-% 
-%     for i = 1:numel(locs_Rwave)-1 %1-16
-%        % if i+1 <= numel(locs_Rwave) %17
-%             lowerBound = (locs_Rwave(i)-120);
-%             upperBound = locs_Rwave(i);
-% 
-%             for j = lowerBound:upperBound
-%                 if j<=numel(real_pksQ) %21
-%                     realQvalue= [realQvalue; real_pksQ(j)];
-%                 end
-% 
-%             end
-%       %  end
-%     end
-
-
-
-
-%                 if ((pksR>lowerBound) & (pksR < upperBound))
-%                     realQvalue = max(pksQ(locs_Qwave >= real_locsQ_wave(i) & locs_Qwave < real_locsQ_wave(i+1)));
-%                     maxSignal = [maxSignal; realQvalue];
-%                     end
-%Aqui hay algo que no esta bien
 
 
 
@@ -172,88 +111,6 @@ heartbeats = (numel(pksR)*6) ;%como estamos muestreando 10seg la señal,
 %60-100bpm
 
 
-
-
-%TRYING TO DETECT THE Q WAVE
-
-
-     %minPeak=-max(pksMin)% para que me coja el valor más negativo
-     %maxPeak= max(pksR);
-%picos maximos y minimos para calcular la media
-
-
-
-
-%     amplitude = maxPeak - minPeak; %porque el mínimo será negativo
-%     y = peak2peak(pksR)
-    
-
-
-% 
-%     Buscamos los picos de Q que estarán entre -0.2mV and -0.5mV
-%     [pksQ,min_locs] = findpeaks(-ECG_data,'MinPeakDistance',40);
-%     locs_Qwave = min_locs(ECG_data(min_locs)>-0.5 & ECG_data(min_locs)<-0.2);
-%     %disp(pksQ)
-% 
-% 
-
-
-
-%
-%     subplot(2,2,2);
-%     plot(locs_Qwave,ECG_data(locs_Qwave),'rs','MarkerFaceColor','g');
-%     grid on
-%     title('Q wave')
-%     xlabel('Samples')
-%     ylabel('Voltage(mV)')
-% 
-%     subplot(2,2,3);
-%     plot(locs_Rwave,ECG_data(locs_Rwave),'rv','MarkerFaceColor','r');
-%     grid on
-%     title('R wave')
-%     xlabel('Samples')
-%     ylabel('Voltage(mV)')
-% 
-%     subplot(2,2,4);
-%     plot(locs_Swave,ECG_data(locs_Swave),'rs','MarkerFaceColor','b');
-%     grid on
-%     title('S wave')
-%     xlabel('Samples')
-%     ylabel('Voltage(mV)')
-% 
-%     %disp('Ondas R: ');
-%     %fprintf('%f\n', pksR);
-%     %disp('Amplitud max: ');
-%     %fprintf('%f\n', maxPeak);
-% 
-%     minPeakS = min(ECG_data(locs_Swave))%-max(pksS); %como esta invertido coger el - (valor máximo) = valor minimo
-%     minPeakQ = min(ECG_data(locs_Qwave));% para que los valores de Q se encuentren entre -0.2 y -0.5
-% 
-%     %disp('Ondas Q: ');
-%     %fprintf('%f\n', ECG_data(locs_Qwave));
-%     disp('Amplitud min Q: ');
-%     fprintf('%f\n', minPeakQ);
-%      disp('Amplitud min S: ');
-%     fprintf('%f\n', minPeakS);
-%     
-%     if(minPeakS>minPeakQ)
-%         minPeak = minPeakQ;
-%     else
-%         minPeak = minPeakS;
-%    end
-%     
-
-
-
-%     disp('Amplitud total')
-%     fprintf('%f\n', maxPeak)
-%     disp('+')
-%     fprintf('%f\n', minPeak)
-%     disp('=')
-%     fprintf('%f\n', amplitude) %-> parece que todo funciona bien
-
-   
-  
     %seguimos con la funcion
     header = {};
 
