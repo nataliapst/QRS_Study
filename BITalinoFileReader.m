7998ecc2ccd0fcb595fe480d422c5ec1c7a75535
nataliapst <79333938+nataliapst@users.noreply.github.com>
2023-06-23 14:10:30 +0200

    Update BITalinoFileReader.m
    New way go getting the amplitude
    
    
    Signed-off-by: nataliapst <79333938+nataliapst@users.noreply.github.com>

--- BITalinoFileReader.m
+++ BITalinoFileReader.m
@@ -42,16 +42,22 @@ function[data,t,header, heartbeats, amplitud_media_real] = BITalinoFileReader(fi
 
 um_y = 6*mean(abs(ECG_data_update)); %De esta manera escogemos los picos que esten por encima de un umbral
 um_x= 0.5*1000; %CORROBORAR SI ES 1100
+% 
+% pksR = zeros(1, length(ECG_data_update-20000));
+% locs_Rwave= zeros(1, length(ECG_data_update-20000)); 
+% widthR= zeros(1, length(ECG_data_update-20000)); 
+% 
 
-[pksR, locs_Rwave, widthR] = findpeaks(ECG_data_update(10000:50000), 'MinPeakWidth',10,'MinPeakDistance',um_x,'MinPeakProminence',0.35);
+[pksR, locs_Rwave, widthR]= findpeaks(ECG_data_update(10000:(length(ECG_data_update)-10000)), 'MinPeakWidth',10,'MinPeakDistance',um_x,'MinPeakProminence',0.35);
+%[pksR, locs_Rwave, ~] = findpeaks(ECG_data_update(10000:50000), 'MinPeakWidth',10,'MinPeakDistance',um_x,'MinPeakProminence',0.35);
 %'MinPeakProminence',0.35 pq lo dijo sergio en el paper-> hay q buscar en que se basó, pero parece que ayuda.
 
 %findpeaks(ECG_data(25000:35000), 'MinPeakWidth',30,'MinPeakDistance',um_x)
-[pksS, locs_Swave, widthS]= findpeaks(-ECG_data_update(10000:50000), 'MinPeakWidth',100,'MinPeakDistance',um_x,'MinPeakProminence',0.35);
+[pksS, locs_Swave, widthS]= findpeaks(-ECG_data_update(10000:(length(ECG_data_update)-10000)), 'MinPeakWidth',100,'MinPeakDistance',um_x,'MinPeakProminence',0.35);
 
 % - - - - - - - - - - - - - - - - - - - - - 
 
-[pksAll, locs_Allwaves] = findpeaks(ECG_data_update(10000:50000));
+[pksAll, locs_Allwaves] = findpeaks(ECG_data_update(10000:(length(ECG_data_update)-10000)));
 %Aqui vamos a guardar todos los máximos que encuentra la señal invertida en el eje x
 
 pksQ = [];
@@ -87,33 +93,85 @@ end
 %Aqui lo que hacemos es escoger el máximo que haya anterior al segmento R,
 %de manera que realmente estamos guardando los picos Q.
 
-
-arrays = [length(pksQ), length(pksR), length(pksS)];
-tam = arrays(1);
-
-for b = 1:length(arrays)
-    if tam > arrays(b)
-        %print(b)
-        tam = arrays(b);
-        
-    end
-end
-
-amplitud=zeros(length(tam), 1);
+amplitud=[];
+minimos=[];
 amplitud_media=0;
+prev_r_wave = 0;
 
-for c = 1:tam
-    amplitud(c) = pksR(c)- pksS(c);
-    amplitud_media = amplitud_media + (pksR(c)- pksS(c));
-end
+for b = 1:numel(pksAll)
+    for i = 1:numel(pksR)
+        if (locs_Rwave(i) - prev_r_wave >= 600) 
+            if (pksAll(b)==pksR(i)) && (locs_Rwave(i) - prev_r_wave >= 600) 
+        
+               inicio= pksR(i) - 80;
+               fin=pksR(i) +120;
+    
+               pksIntervalo = pksAll((locs_Allwaves >= inicio) & (locs_Allwaves <= fin));
+               [minPico, ~] = min(pksIntervalo);
+               minimos=[minimos;min(pksIntervalo) ]
+    
+               amplitud= [amplitud; (pksR(i)-minPico)];
+               amplitud_media= amplitud_media + (pksR(i)-minPico);
+    
+            end
+        end
+        prev_r_wave = locs_Rwave(i);
+    end
 
-amplitud_media_real = amplitud_media/tam;
+end;
+
+amplitud_media_real = amplitud_media/length(amplitud);
+
+
+% arrays = [length(pksQ), length(pksR), length(pksS)];
+% tam = arrays(1);
+% for b = 1:length(arrays)
+%     if tam > arrays(b)
+%         %print(b)
+%         tam = arrays(b);
+%        
+%     end
+% end
+% 
+% prev_r_wave = 0;  % Variable para almacenar el valor anterior de locs_Rwave
+% amplitud=[];
+% amplitud_media=0;
+% cont=1; 
+% 
+
+%     for c = 1:tam   
+%         if (cont<tam) && (locs_Swave(c) > (locs_Rwave(cont) + 150)) 
+%             cont=cont+1;
+%         end
+%     
+%         if (cont<tam) && (locs_Swave(c) <= (locs_Rwave(cont) + 150)) %&& (locs_Rwave(cont) - prev_r_wave >= 600) 
+%             amplitud=[amplitud; (abs(pksR(cont)-pksS(c)))];
+%             amplitud_media= amplitud_media + (abs(pksR(cont)-pksS(c)));
+%        
+%         end
+%      
+%         prev_r_wave = locs_Rwave(cont);
+%         
+%         if(cont==tam)
+%             break;
+%         else
+%             cont=cont+1;
+%         end 
+%          
+%     end
+% 
+% amplitud_media_real = amplitud_media/length(amplitud);
+
+
+
+
 
 %heartbeats = latidos / min = duracion complejo QRS/ min
-%heartbeats = latidos / min = duracion complejo QRS/ min
 
-heartbeats = (numel(pksR)*1.5) ;%como estamos muestreando 40seg la señal (60seg -10seg del principio -10seg del final),
-% %si multiplicamos el numerador y el denominador por (60/40 =) 1.5 tenemos los latidos
+duracionECG = ((length(ECG_data_update)-20000)/1000);
+heartbeats = (numel(pksR)*(60/duracionECG) );
+%como estamos muestreando 10seg la señal,
+% %si multiplicamos el numerador y el denominador por 6 tenemos los latidos
 % por minuto.
 %Un latido es un intervalo RR por eso (numel(pksR)-1), asi contamos intervalos y no número de picos R encontrados
 %60-100bpm
@@ -147,6 +205,3 @@ heartbeats = (numel(pksR)*1.5) ;%como estamos muestreando 40seg la señal (60seg
     else
         t = (1:length(data))/srate;
     end
-
-    return;
-end
