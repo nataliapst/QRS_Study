6ffe859cf8b7b4134c2a2e6fc4e9d34561882213
nataliapst <79333938+nataliapst@users.noreply.github.com>
2023-06-28 11:04:29 +0200

    Update EstudioQRS.m
    Changes in correlations
    
    Signed-off-by: nataliapst <79333938+nataliapst@users.noreply.github.com>

--- EstudioQRS.m
+++ EstudioQRS.m
@@ -43,7 +43,7 @@ ar=ls(path); %para listar los contenidos
       amplitud_array = [];
 
       for j= 1:numel(archivoOrdenado)
-          %disp(j)
+           disp(j)
           %fprintf(stringArchivo(j));
           %fprintf(strcat(stringArchivo(j), '.txt'))
           [~,~,~,heartbeats, amplitud]= BITalinoFileReader(strcat(stringArchivo(j), '.txt'));
@@ -52,27 +52,28 @@ ar=ls(path); %para listar los contenidos
          amplitud_array(j,1)= amplitud;
          
       end
-      
-        
+   
 
     % % Calcular la correlación
     % 
      fprintf("\nAmplitude-Heartbeats correlation")
      [R1,P1] = corrcoef(amplitud_array,heartbeats_array);
-    % %R1 =0.1824 -> correlacion
-    % %P1 =0.1251 
+    % %R1 = 0.1783-> correlacion
+    % %P1 =0.1398
 
-%     [~, p, ~, ~] = ttest2(amplitud_array,heartbeats_array);
-%     p = 7.2393e-88
-%     
+    [h,p]=ttest(amplitud_array, heartbeats_array)
+
+%h =1 RECHAZAMOS LA HIPOTESIS NULA
+%p =2.4503e-53
+
 %     figure
 %     p = polyfit(amplitud_array,heartbeats_array,1)
-%     p =  8.6613   73.0617
+%     p =   6.1936   71.3398
 %     x_regression = linspace(min(amplitud_array), max(amplitud_array), 100);
 %     y_regression = polyval(p, x_regression);
 %     plot(amplitud_array, heartbeats_array, 'o', x_regression, y_regression);
 %     xlabel('Amplitude');
-%     ylabel('Heartbeats');
+%     ylabel('Heart Rate');
 %     title('Lineal regression');
 
 
@@ -80,17 +81,24 @@ altura = [1.61, 1.65, 1.68, 1.80, 1.74, 1.65, 1.65, 1.85, 1.70, 1.80, 1.79, 1.63
 peso = [65, 100, 75, 92, 75, 60, 54, 83, 85, 70, 66, 53, 72, 75, 55, 78, 57, 56, 68, 58, 61, 58, 55, 67, 53, 75, 74, 96, 79, 79, 79, 57, 48, 85, 60, 60, 53, 58, 58, 77, 47, 93, 75, 60, 75, 65, 55, 49, 76, 57, 42, 56, 60, 54, 80, 65, 80, 58, 62, 66, 50, 62, 50, 81, 83, 78, 55, 60, 85, 75, 55, 63, 85, 55, 68, 94, 63, 100, 64, 74, 71, 65, 78, 79, 85, 68, 79, 73, 75, 66.3, 98, 78, 82, 68, 61, 80, 76, 53, 100];
 edad = [18, 49, 18, 58, 48, 22, 39, 22, 22, 21, 19, 20, 21, 20, 20, 21, 23, 22, 22, 22, 22, 21, 21, 21, 21, 18, 19, 26, 24, 29, 22, 19, 21, 21, 21, 22, 22, 21, 22, 21, 19, 26, 26, 19, 19, 24, 22, 21, 23, 22, 20, 22, 23, 22, 22, 20, 22, 19, 20, 21, 57, 43, 22, 23, 20, 24, 21, 22, 21, 22, 21, 21, 21, 20, 21, 57, 64, 51, 59, 59, 33, 42, 40, 24, 22, 20, 20, 23, 52, 60, 23, 53, 56, 42, 42, 48, 56, 24, 45];
 
- posiciones_eliminar = [1, 8:27, 33, 42, 45, 57, 73, 84];
+posiciones_eliminar = [1, 8:27, 33, 42, 45, 57, 73,80,84,90];
+
+
+
 % 
 %  altura(posiciones_eliminar) = [];
 %  peso(posiciones_eliminar) = [];
 %  edad(posiciones_eliminar) = [];
 
-women = [2, 3, 6, 7, 32, 34, 36, 37, 38 , 39, 41, 44, 47, 48, 50, 51, 52, 53, 54, 55, 56, 58, 61, 62, 63, 67, 71, 72, 74, 77, 79, 82, 90, 92, 95, 98 ];
+women = [2, 3, 6, 7, 32, 34, 36, 37, 38 , 39, 41, 44, 47, 48, 50, 51, 52, 53, 54, 55, 56, 58, 61, 62, 63, 67, 71, 72, 74, 77, 79, 82, 92, 95, 98 ];
 men = [4, 5, 28, 29, 30, 31, 35, 40, 43, 46, 49, 59, 60, 64, 65, 66, 68, 69, 70, 75, 76, 78, 80, 81, 83, 85, 86, 87, 88, 89, 91, 93, 94, 96, 97, 99];
 
-womeninFolder = [1,2,5,6,11,12,14,15,16,17,19,21,23,24,26:33,36,37,38,42,46,47,48,51,53,56,63,65,68,71];%Posicion en la carpeta de las q son mujeres
-meninFolder = [3,4,7,8,9,10,13,18,20,22,25,34,35,39,40,41,43,44,45,49,50,52,54,55,57:62,64,66,67,69,70,72];
+womeninFolder = [1,2,5,6,11,12,14,15,16,17,19,21,23,24,26:33,36,37,38,42,46,47,48,51,53,55,63, 66,69];%Posicion en la carpeta de las q son mujeres
+meninFolder = [3,4,7,8,9,10,13,18,20,22,25,34,35,39,40,41,43,44,45,49,50,52,54,56,57,58,59,60,61,62,64,65,67,68,70];
+
+pesoAll= peso;
+pesoAll(posiciones_eliminar) = [];
+
 
 alturaWomen=[];
 alturaWomen= altura(women);
@@ -103,60 +111,34 @@ pesoWomen= peso(women);
 
 pesoMen = [];
 pesoMen = peso(men);
+% 
+% edadWomen =[];
+% edadWomen=edad(women);
+% 
+% edadMen=[];
+% edadMen= edad(men);
 
-edadWomen =[];
-edadWomen=edad(women);
-
-edadMen=[];
-edadMen= edad(men);
-
-
-% IMC=[];
-% 
-% alturaAll= altura;
-% alturaAll(posiciones_eliminar) = [];
-% 
-% for k1 = 1:(numel(alturaAll))
-%     IMC=[IMC; (peso(k1)/(alturaAll(k1)^2))]
-% end;
-% 
-% 
-% IMCWomen=[];
-% 
-% for k2 = 1:(numel(alturaWomen))
-%     IMCWomen=[IMCWomen; (pesoWomen(k2)/(alturaWomen(k2)^2))]
-% end;
-% 
-% 
-% 
-% IMCMen=[];
-% 
-% for k3 = 1:(numel(alturaMen))
-%     IMCMen=[IMCMen; (pesoMen(k3)/(alturaMen(k3)^2))]
-% end;
-% 
-
-
-%AQUI 
 
 pesoAll= peso;
 pesoAll(posiciones_eliminar) = [];
+pesoAll = transpose(pesoAll);
 
 fprintf("\nAmplitude-Weigth correlation")
 [R2,P2] = corrcoef(amplitud_array,pesoAll);
-%R2=0.2006
-%P2=0.0911
+%R2=0.2850
+%P2=0.0168
 
-%[~, p, ~, ~] = ttest2(amplitud_array,pesoAll)
-%p =8.3957e-82
+[h,p]=ttest(amplitud_array, pesoAll)
 
+%h =1 RECHAZAMOS LA HIPOTESIS NULA
+%p = 4.6764e-50
 
 % figure
 % p = polyfit(amplitud_array,pesoAll,1)
 % 
 % p =
 % 
-%      9.5880   65.5268
+%      10.0881   61.1355
 % 
 % x_regression = linspace(min(amplitud_array), max(amplitud_array), 100);
 % y_regression = polyval(p, x_regression);
@@ -165,10 +147,6 @@ fprintf("\nAmplitude-Weigth correlation")
 % ylabel('Weight');
 % title('Lineal Regression');
 % 
-
-%there is a significant and meaningful relationship between the amplitud_array
-% and pesoAll. The extremely small p-value supports the existence of a correlation between 
-% the two variables.
 
 amplitudRWomen= []; 
 amplitudWomen = amplitud_array(womeninFolder);
@@ -182,20 +160,24 @@ amplitudMen = amplitud_array(meninFolder);
 pesoMen= []; 
 pesoMen = peso(meninFolder);
 
-
+pesoWomen = transpose(pesoWomen);
 
 fprintf("\nAmplitude-IMC Women correlation")
 [R3,P3] = corrcoef(amplitudWomen,pesoWomen);
 
-%R3= -0.1455
-%P3= 0.3972
-%[~, p, ~, ~] = ttest2(amplitudWomen,pesoWomen) -> p= 3.9517e-40
+%R3=-0.3602
+%P3=  0.0335
+
+[h,p]=ttest(amplitudWomen,pesoWomen)
+%h =1 RECHAZAMOS LA HIPOTESIS NULA
+%p = 1.0452e-24
+
 %figure
 % p = polyfit(amplitudWomen,pesoWomen,1) 
 % 
 % p =
 % 
-%      -6.4066   68.2022
+%       -13.6366   76.8816
 % 
 % x_regression = linspace(min(amplitudWomen), max(amplitudWomen), 100);
 % y_regression = polyval(p, x_regression);
@@ -205,23 +187,22 @@ fprintf("\nAmplitude-IMC Women correlation")
 % title('Women Lineal Regression');
 
 
-%Por lo tanto, podemos afirmar que hay una relación significativa entre la amplitud y el peso de la mujer, aunque la correlación es débil.
-%   -Existe una correlación débil y negativa entre la amplitud y el peso de la mujer.
-%   -La correlación es altamente significativa desde el punto de vista estadístico, con un valor de p muy pequeño.
-%   -A medida que el peso de la mujer aumenta, la amplitud tiende a disminuir, y viceversa.
-
+pesoMen = transpose(pesoMen);
 fprintf("\nAmplitude-IMC Men correlation")
 [R4,P4] = corrcoef(amplitudMen,pesoMen);
-%R4=0.0416
-%P4=0.8097
-%[~, p, ~, ~] = ttest2(amplitudMen, pesoMen) -> p= 1.6564e-44
+%R4= -0.0739
+%P4= 0.6732
+
+[h,p]=ttest(amplitudMen,pesoMen)
+%h =1 RECHAZAMOS LA HIPOTESIS NULA
+%p = 3.6119e-27
 
 %figure
 % p = polyfit(amplitudMen, pesoMen,1)
 % 
 % p =
 % 
-%       1.8773   66.7133
+%       -2.3767   68.8338
 % 
 % x_regression = linspace(min(amplitudMen), max(amplitudMen), 100);
 % y_regression = polyval(p, x_regression);
@@ -230,10 +211,7 @@ fprintf("\nAmplitude-IMC Men correlation")
 % ylabel('Weight');
 % title('Men Lineal Regression');
 
-%Por lo tanto, podemos afirmar que hay una relación significativa entre la amplitud y el 
-% peso de los hombres, aunque la correlación es débil.
-%A medida que el peso de los hombres aumenta, la amplitud también tiende a aumentar, y viceversa.
-
    end
  end
 
+    
